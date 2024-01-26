0db5f19359cadf2954eb39210963f4a93ca091b1
elhaou nouhaila <elhaounouhaila@gmail.com>
2024-01-26 18:01:31 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-
   get'about', to: 'pages#about'
    devise_for :users
    root to: 'pages#home'
-   get 'payment', to: 'payment#execute'
    # get 'login', to: 'devise/sessions#new'  # Update this line
    # get 'sign_up', to: 'devise/registrations#new'
    get 'pitches', to: 'pitches#index'
-end
+
+
+   resources :bookings, only: [:new, :create]
+    resources :pitches, only: [:index]
+
+
+ end
