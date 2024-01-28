7ce0b67e997835da4e604f97a89220768f1a146b
ayoub elkabli <125612305+Ayoubelkabli@users.noreply.github.com>
2024-01-28 01:17:38 +0100

    fix: home

---
+++ db/migrate/20240127221048_add_img_desc.rb
@@ -0,0 +1,4 @@
+class AddImgDesc < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 
 Stade.destroy_all
 
-Stade.create(name: "Arena City",desc: "Explore a versatile place that celebrates outdoor living in a sustainable urban setting ", img: "https://res.cloudinary.com/dw39u26vq/image/upload/v1706190952/ARENA_4_bwngg4.jpg")
-Stade.create(name: "Atlantic Club",desc: "Discover the beauty of the coast at Atlantic Club, your ultimate destination.", img:  "https://res.cloudinary.com/dw39u26vq/image/upload/v1706181376/atlantique_hrn3uk.jpg")
-Stade.create(name: "Ginga Sport",desc: "Ginga Sport offers a unique experience, combining nature and relaxation.", img: "https://res.cloudinary.com/dw39u26vq/image/upload/v1706191582/ginga_2_esutlg.jpg")
+Stade.create(name: "Arena City",des: "Explore a versatile place that celebrates outdoor living in a sustainable urban setting ", img: "https://res.cloudinary.com/dw39u26vq/image/upload/v1706190952/ARENA_4_bwngg4.jpg")
+Stade.create(name: "Atlantic Club",des: "Discover the beauty of the coast at Atlantic Club, your ultimate destination.", img:  "https://res.cloudinary.com/dw39u26vq/image/upload/v1706181376/atlantique_hrn3uk.jpg")
+Stade.create(name: "Ginga Sport",des: "Ginga Sport offers a unique experience, combining nature and relaxation.", img: "https://res.cloudinary.com/dw39u26vq/image/upload/v1706191582/ginga_2_esutlg.jpg")
 
-Stade.create(name: "City foot",desc: "City Foot offers a dynamic urban gaming experience in the heart of the city. With modern and well-maintained football fields.", img: "https://res.cloudinary.com/ddiq9sddp/image/upload/v1706104882/city-foot-5-club-de-sport-casablanca-517_ozc4sf.jpg")
+Stade.create(name: "City foot",des: "City Foot offers a dynamic urban gaming experience in the heart of the city. With modern and well-maintained football fields.", img: "https://res.cloudinary.com/ddiq9sddp/image/upload/v1706104882/city-foot-5-club-de-sport-casablanca-517_ozc4sf.jpg")
