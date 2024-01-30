39235fb95f0cb7e31e08a938ea0a6948cf1fe73e
omar anis <omar-anis466@hotmail.com>
2024-01-30 11:45:18 +0100

    cache

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,11 +1,6 @@
 PayPal::SDK.configure(
   mode: 'sandbox', # 'sandbox' pour le mode de test, 'live' pour la production
-<<<<<<< HEAD
-  # client_id: TUUY3G6XLWLN2,
-  # client_secret: 790703997289
-=======
   client_id: "TUUY3G6XLWLN2",
   client_secret: 790703997289
->>>>>>> b6045f6753608239638d911c16ea9ec62a675d58
 
 )
