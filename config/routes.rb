5f93037cb9440cf9fffcac2d76721eb6a5e2c62f
omar anis <omar-anis466@hotmail.com>
2024-01-30 15:01:17 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get'about', to: 'pages#about'
-   devise_for :users
-   root to: 'pages#home'
-   # get 'login', to: 'devise/sessions#new'  # Update this line
-   # get 'sign_up', to: 'devise/registrations#new'
-   get 'pitches', to: 'pitches#index'
 
+  devise_for :users
+  root to: 'pages#home'
 
-   resources :bookings, only: [:new, :create]
-    resources :pitches, only: [:index]
+  resources :bookings, only: [:new, :create]
+  resources :pitches, only: [:index]
 
+  get 'about', to: 'pages#about'
 
  end
