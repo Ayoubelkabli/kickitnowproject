b1bc7fc7d7c1309cf389cf4cefcd9a4d7e784b59
elhaou nouhaila <elhaounouhaila@gmail.com>
2024-02-05 17:40:18 +0100

    add a new home page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
 import "@popperjs/core"
 import "bootstrap"
-// app/javascript/application.js
 
-// ...
+import './controllers';
 import './mode';
-import 'stylesheets/application.scss'; // Import your styles if needed
-import "@hotwired/turbo-rails";
