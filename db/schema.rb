6bcdb4030361e44ffd7619c2ae2cc237e81d1b3c
ayoub elkabli <125612305+Ayoubelkabli@users.noreply.github.com>
2024-02-08 17:11:13 +0100

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_22_210621) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_08_145835) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,12 +24,32 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_22_210621) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "chatroom_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "stades", force: :cascade do |t|
     t.string "name"
     t.string "desc"
     t.string "img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "capacity"
+    t.string "surface_type"
+    t.string "availability"
+    t.string "map_url"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,4 +68,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_22_210621) do
 
   add_foreign_key "bookings", "stades"
   add_foreign_key "bookings", "users"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
 end
